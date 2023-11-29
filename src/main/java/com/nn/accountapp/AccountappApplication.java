0209b57df764e009d36924cf7682338fb4ee138d
lgebicki <geblukasz@gmail.com>
2023-11-29 20:40:10 +0100

    [NN-00] Spring initializer package added

---
+++ src/main/java/com/nn/accountapp/AccountappApplication.java
@@ -0,0 +1,13 @@
+package com.nn.accountapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountappApplication.class, args);
+	}
+
+}
